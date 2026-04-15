0a8d9bbac81bebdc017e8ae33061aace9c5134f7
sinkpig <sinkpig00@gmail.com>
2026-04-15 20:05:25 -0300

    feat: added homeImages object

---
+++ src/data/homeImages.js
@@ -0,0 +1,42 @@
+export const homeImages = [
+  {
+    src: '/Shieni.png',
+    alt: '',
+  },
+  {
+    src: '/Concurso Vograce.png',
+    alt: '',
+  },
+  {
+    src: '/Dot Attack.png',
+    alt: '',
+  },
+  {
+    src: '/Mini-study-Animation-Ten.gif',
+    alt: '',
+  },
+  {
+    src: '/Renard Attack.png',
+    alt: '',
+  },
+  {
+    src: '/Berry Attack.png',
+    alt: '',
+  },
+  {
+    src: '/Attack 2 Dip - sinkpig.png',
+    alt: '',
+  },
+  {
+    src: '/Character Sheet Byron.png',
+    alt: '',
+  },
+  {
+    src: '/Cell Shade.png',
+    alt: '',
+  },
+  {
+    src: '/Characters.png',
+    alt: '',
+  },
+]
