af10f56db57e1c462221e0f81d019c6c634f5c02
sinkpig <sinkpig00@gmail.com>
2026-04-16 10:49:23 -0300

    chore: refactored homeImages + fixed Nav naming convention

--- src/data/homeImages.js
+++ src/data/homeImages.js
@@ -1,42 +1,48 @@
-export const homeImages = [
-  {
-    src: '/Shieni.png',
-    alt: '',
-  },
-  {
-    src: '/Concurso Vograce.png',
-    alt: '',
-  },
-  {
-    src: '/Dot Attack.png',
-    alt: '',
-  },
-  {
-    src: '/Mini-study-Animation-Ten.gif',
-    alt: '',
-  },
-  {
-    src: '/Renard Attack.png',
-    alt: '',
-  },
-  {
-    src: '/Berry Attack.png',
-    alt: '',
-  },
-  {
-    src: '/Attack 2 Dip - sinkpig.png',
-    alt: '',
-  },
-  {
-    src: '/Character Sheet Byron.png',
-    alt: '',
-  },
-  {
-    src: '/Cell Shade.png',
-    alt: '',
-  },
-  {
-    src: '/Characters.png',
-    alt: '',
-  },
-]
+export const homeImages = {
+  firstColumn: [
+    {
+      src: '/Shieni.png',
+      alt: '',
+    },
+    {
+      src: '/Concurso Vograce.png',
+      alt: '',
+    },
+    {
+      src: '/Dot Attack.png',
+      alt: '',
+    },
+  ],
+  secondColumn: [
+    {
+      src: '/Mini-study-Animation-Ten.gif',
+      alt: '',
+    },
+    {
+      src: '/Renard Attack.png',
+      alt: '',
+    },
+    {
+      src: '/Berry Attack.png',
+      alt: '',
+    },
+    {
+      src: '/Attack 2 Dip - sinkpig.png',
+      alt: '',
+    },
+  ],
+  thirdColumn: [
+    {
+      src: '/Character Sheet Byron.png',
+      alt: '',
+    },
+    {
+      src: '/Cell Shade.png',
+      alt: '',
+    },
+    {
+      src: '/Characters.png',
+      alt: '',
+    },
+  ]
+}
