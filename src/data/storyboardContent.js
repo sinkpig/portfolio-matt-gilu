b3a37157652211c4a7ee23bd1a2d6c6a9d07a932
sinkpig <sinkpig00@gmail.com>
2026-05-14 12:39:04 -0300

    feat: added new section in storyboard

--- src/data/storyboardContent.js
+++ src/data/storyboardContent.js
@@ -3,17 +3,64 @@ export const storyboardContent = {
   color: 'blue',
   section: [
     {
+      title: 'The Forest Mercy',
+      subtitle: 'Lorem ipsum',
+      link: 'https://docs.google.com/presentation/d/e/2PACX-1vTyAc-RyelFfcNRB-6h2g1SAG9Ph1inr_qZ942OGIkcznfDrrMmP5yPc1MKb7DNiMlOso9sCbcH32Lz/pubembed?start=false&loop=true&delayms=3000',
+      images: [
+        {
+          src: '/storyboard/aydian-marlon-view-1.jpg',
+          alt: ''
+        },
+        {
+          src: '/storyboard/aydian-marlon-view-2.jpg',
+          alt: ''
+        },
+        {
+          src: '/storyboard/aydian-marlon-view-3.jpg',
+          alt: ''
+        },
+        {
+          src: '/storyboard/aydian-marlon-view-4.jpg',
+          alt: ''
+        },
+        {
+          src: '/storyboard/aydian-marlon-view-5.jpg',
+          alt: ''
+        },
+        {
+          src: '/storyboard/aydian-marlon-view-6.jpg',
+          alt: ''
+        },
+        {
+          src: '/storyboard/aydian-marlon-view-7.jpg',
+          alt: ''
+        },
+        {
+          src: '/storyboard/aydian-marlon-view-8.jpg',
+          alt: ''
+        },
+        {
+          src: '/storyboard/aydian-marlon-view-9.jpg',
+          alt: ''
+        },
+        {
+          src: '/storyboard/aydian-marlon-view-10.jpg',
+          alt: ''
+        },
+      ]
+    },
+    {
       title: 'Angel Trap',
       subtitle: 'Lorem Ipsum dolor sit amet',
       description: "Hi! I'm Gabriela Chumpitaz, a Peruvian illustrator and freelance visual development artist. I've had the opportunity to work on some incredible projects, including award-winning ones supported by DAFO (Peru). I'm deeply passionate about storytelling and love bringing moments. Hi! I'm Gabriela Chumpitaz, a Peruvian illustrator and freelance visual development artist. I've had the opportunity to work on some incredible projects, including award-winning ones supported by DAFO (Peru). I'm deeply passionate about storytelling and love bringing moments.",
       link: 'https://docs.google.com/presentation/d/e/2PACX-1vQjA_rzPIDhA02FUXBlsivQCL7cn5hOruQk-fLlEp2aJFaWtzF8n41B34ux2l2veHzpC3CBjTqZqXTo/pubembed?start=false&loop=true&delayms=3000',
       images: [
         {
-          src: '/storyboard/shieni-thumbnails.jpg',
+          src: '/storyboard/shieni-view-1.jpg',
           alt: ''
         },
         {
-          src: '/storyboard/shieni-thumbnails-2.jpg',
+          src: '/storyboard/shieni-view-2.jpg',
           alt: ''
         }
       ]
