43151c16360f39f580a72a92bc12813eee6c7121
sinkpig <sinkpig00@gmail.com>
2026-06-11 22:19:38 -0300

    feat: added final texts

--- src/data/textContent.js
+++ src/data/textContent.js
@@ -7,7 +7,7 @@ export const textContent = {
   },
   about: {
     title: "About me",
-    paragraph: "Hi! I'm Gilu, a passionate artist who loves making fleshed out characters and currently on a journey to turn those stories into animated realities!\nMy projects so far are personal. It has always been my passion to create captivating characters and compelling stories where we can follow their growth, reflecting that progress on their design and creating a visual emotional impact.\nAnd little by little, I hope to someday reach my goal! Let's work together!",
+    paragraph: "Hi! I'm Gilu Matt and I'm a Peruvian illustrator and freelance storyboard artist. I graduated from the institute Toulouse Lautrec back in 2020 after which I began working on visual development for E-learning assets up until 2024.\n\nI've been commissioned for artwork to illustrate digital books since 2022, which also gave me time to upgrade my artistic skills while taking courses with Schoolism. After that, in 2025 I took a course by Sophia Magallanes on Storytelling Language which allowed me to better conceptualize visual storytelling, both on my illustrations and in storyboardsP.\n\nI'm greedy for knowledge, so I hope to keep learning and working with more people on creating stories!",
     src: "/about/sketch-portafolio-about.png",
     linkText: "Download resume",
     link: "/about/cv-gianni-barbieri.pdf",
