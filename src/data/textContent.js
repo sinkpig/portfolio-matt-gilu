0fa4deae5da4d375e5128dd127737bac6e1fd036
sinkpig <103704017+sinkpig@users.noreply.github.com>
2026-06-22 17:29:20 -0300

    hotfix: Update textContent.js

--- src/data/textContent.js
+++ src/data/textContent.js
@@ -7,7 +7,7 @@ export const textContent = {
   },
   about: {
     title: "About me",
-    paragraph: "Hi! I'm Gilu Matt and I'm a Peruvian illustrator and freelance storyboard artist. I graduated from the institute Toulouse Lautrec back in 2020 after which I began working on visual development for E-learning assets up until 2024.\n\nI've been commissioned for artwork to illustrate digital books since 2022, which also gave me time to upgrade my artistic skills while taking courses with Schoolism. After that, in 2025 I took a course by Sophia Magallanes on Storytelling Language which allowed me to better conceptualize visual storytelling, both on my illustrations and in storyboardsP.\n\nI'm greedy for knowledge, so I hope to keep learning and working with more people on creating stories!",
+    paragraph: "Hi! I'm Gilu Matt and I'm a Peruvian illustrator and freelance storyboard artist. I graduated from the institute Toulouse Lautrec back in 2020 after which I began working on visual development for E-learning assets up until 2024.\n\nI've been commissioned for artwork to illustrate digital books since 2022, which also gave me time to upgrade my artistic skills while taking courses with Schoolism. After that, in 2025 I took a course by Sophia Magallanes on Storytelling Language which allowed me to better conceptualize visual storytelling, both on my illustrations and in storyboards.\n\nI'm greedy for knowledge, so I hope to keep learning and working with more people on creating stories!",
     src: "/about/sketch-portafolio-about.png",
     linkText: "Download resume",
     link: "/about/cv-gianni-barbieri.pdf",
